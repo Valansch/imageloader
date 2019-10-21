79378926cefcbb486e081409f48d26b9f61aa5a7
Maik Wild <mail@maikwild.de>
2019-10-21 10:35:07 +0200

    Fixed linting errors

--- imageloader.py
+++ imageloader.py
@@ -1,15 +1,17 @@
-import requests
 import argparse
+import logging
 import os
 import re
 import sys
-import logging
 from concurrent.futures import ThreadPoolExecutor
+
+import requests
+
 
 def download(url, dest):
     try:
         response = requests.get(url)
-        if not "image" in response.headers["Content-Type"]:
+        if "image" not in response.headers["Content-Type"]:
             logging.warning(f"{url} - Response object not of type image.")
             return False
     except requests.exceptions.RequestException:
@@ -24,25 +26,47 @@ def download(url, dest):
         logging.warning(f"{url} - Image not found on server.")
         return False
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Download images in bulk.')
-    parser.add_argument('input_file', metavar='<Input file>', type=str, help='The file containing urls of pictures to download.')
-    parser.add_argument('-o', '--output_path', metavar='<Output folder>', default=os.getcwd(), type=str, help='Where the images will be stored.')
-    parser.add_argument('-t', '--threads', metavar='<Output folder>', default=8, type=int, help='Number of concurrent download threads.')
+    parser = argparse.ArgumentParser(description="Download images in bulk.")
+    parser.add_argument(
+        "input_file",
+        metavar="<Input file>",
+        type=str,
+        help="The file containing urls of pictures to download.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_path",
+        metavar="<Output folder>",
+        default=os.getcwd(),
+        type=str,
+        help="Where the images will be stored.",
+    )
+    parser.add_argument(
+        "-t",
+        "--threads",
+        metavar="<Output folder>",
+        default=8,
+        type=int,
+        help="Number of concurrent download threads.",
+    )
     return vars(parser.parse_args())
+
 
 def read_unique_lines(file_name):
     lines = []
     with open(file_name, "r") as file:
         line = file.readline()
         while line:
-            trimmed = re.sub(r'(\s)+', '', line)
+            trimmed = re.sub(r"(\s)+", "", line)
             if len(trimmed) > 0:
                 lines.append(trimmed)
             line = file.readline()
-    
-    lines = list(dict.fromkeys(lines)) #Remove duplicates
+
+    lines = list(dict.fromkeys(lines))  # Remove duplicates
     return lines
+
 
 def validate_arguments(arguments):
     input_file_path = os.path.realpath(arguments["input_file"])
@@ -62,7 +86,8 @@ def validate_arguments(arguments):
         sys.exit(-1)
     return True
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     arguments = parse_arguments()
     validate_arguments(arguments)
@@ -71,7 +96,3 @@ if __name__ == '__main__':
     for i, url in enumerate(urls, start=1):
         output_file = os.path.join(arguments["output_path"], str(i) + ".jpg")
         executor.submit(download, url, output_file)
-
-
-
-
--- tests/test_imageloader.py
+++ tests/test_imageloader.py
@@ -1,20 +1,24 @@
 import os
-import pytest
-from imageloader import download, read_unique_lines, validate_arguments
-from mock import patch
 from sys import exit
 
+from mock import patch
+
+from imageloader import download, read_unique_lines, validate_arguments
 
 test_input_file_name = "test_input.txt"
-test_input_file_content = " https://goog le.com\nhttps://google.com\n https://example.org"
+test_input_file_content = (
+    " https://goog le.com\nhttps://google.com\n https://example.org"
+)
 test_output_folder_path = "test_output"
 test_file_output_file_path = "test_output.jpg"
+
 
 def rm(inode_path):
     if os.path.isfile(inode_path):
         os.remove(inode_path)
-    else: 
+    else:
         os.rmdir(inode_path)
+
 
 def setup_module():
     if os.path.exists(test_file_output_file_path):
@@ -24,6 +28,7 @@ def setup_module():
     with open(test_input_file_name, "w") as test_input_file:
         test_input_file.write(test_input_file_content)
 
+
 def teardown_module():
     if os.path.exists(test_file_output_file_path):
         rm(test_file_output_file_path)
@@ -32,41 +37,48 @@ def teardown_module():
     if os.path.exists(test_output_folder_path):
         rm(test_output_folder_path)
 
+
 def test_download_not_found():
     assert not download("https://server.does.not.exists", test_file_output_file_path)
 
+
 def test_download_url_404():
-    assert not download("https://google.com/path_does_not_exist", test_file_output_file_path)
+    assert not download(
+        "https://google.com/path_does_not_exist", test_file_output_file_path
+    )
+
 
 def test_download_url_success():
     assert download("https://picsum.photos/200", test_file_output_file_path)
     assert os.path.exists(test_file_output_file_path)
     assert os.stat(test_file_output_file_path).st_size > 0
 
+
 def test_read_unique_lines():
     lines = read_unique_lines(test_input_file_name)
     assert lines == ["https://google.com", "https://example.org"]
 
 
-@patch('sys.exit')
+@patch("sys.exit")
 def test_validate_arguments(mockExit):
 
     os.mkdir(test_output_folder_path)
-    arguments = {"input_file": test_input_file_name, 'output_path': test_output_folder_path}
-    assert validate_arguments(arguments) # Everything working
+    arguments = {
+        "input_file": test_input_file_name,
+        "output_path": test_output_folder_path,
+    }
+    assert validate_arguments(arguments)  # Everything working
 
     # Output folder missing
     rm(test_output_folder_path)
     validate_arguments(arguments)
-    rv = mockExit.call_args_list[0].args[0] 
-    assert mockExit.call_count > 0 and mockExit.call_args_list[0].args[0] == -1 
+    assert mockExit.call_count > 0 and mockExit.call_args_list[0].args[0] == -1
 
     # Input file missing
     os.mkdir(test_output_folder_path)
     rm(test_input_file_name)
     validate_arguments(arguments)
-    assert mockExit.call_args_list[1].args[0] == -1 
-    
+    assert mockExit.call_args_list[1].args[0] == -1
 
     # Input file is a directory
     os.mkdir(test_input_file_name)
