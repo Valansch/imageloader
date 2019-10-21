790b9a2c0b9b572e944fafa3fdb92f9d67dd574b
Maik Wild <mail@maikwild.de>
2019-10-21 16:44:37 +0200

    rewirte validate arguments into multiple tests

--- tests/test_imageloader.py
+++ tests/test_imageloader.py
@@ -1,5 +1,4 @@
 import os
-from sys import exit
 
 from mock import patch
 
@@ -11,29 +10,28 @@ test_input_file_content = (
 )
 test_output_folder_path = "test_output"
 test_file_output_file_path = "test_output.jpg"
+arguments = {"input_file": test_input_file_name, "output_path": test_output_folder_path}
 
 
 def rm(inode_path):
-    if os.path.isfile(inode_path):
-        os.remove(inode_path)
-    else:
-        os.rmdir(inode_path)
+    if os.path.exists(inode_path):
+        if os.path.isfile(inode_path):
+            os.remove(inode_path)
+        else:
+            os.rmdir(inode_path)
 
 
 def setup_module():
-    if os.path.exists(test_file_output_file_path):
-        rm(test_file_output_file_path)
-    if os.path.exists(test_output_folder_path):
-        rm(test_output_folder_path)
+    rm(test_file_output_file_path)
+    rm(test_output_folder_path)
+    os.mkdir(test_output_folder_path)
     with open(test_input_file_name, "w") as test_input_file:
         test_input_file.write(test_input_file_content)
 
 
 def teardown_module():
-    if os.path.exists(test_file_output_file_path):
-        rm(test_file_output_file_path)
-    if os.path.exists(test_input_file_name):
-        rm(test_input_file_name)
+    rm(test_file_output_file_path)
+    rm(test_input_file_name)
     if os.path.exists(test_output_folder_path):
         rm(test_output_folder_path)
 
@@ -59,38 +57,35 @@ def test_read_unique_lines():
     assert lines == ["https://google.com", "https://example.org"]
 
 
-@patch("sys.exit")
-def test_validate_arguments(mockExit):
-
-    os.mkdir(test_output_folder_path)
-    arguments = {
-        "input_file": test_input_file_name,
-        "output_path": test_output_folder_path,
-    }
+def test_validate_arguments_valid():
     assert validate_arguments(arguments)  # Everything working
 
-    # Output folder missing
+
+@patch("sys.exit")
+def test_validate_arguments_no_output_folder(mockExit):
     rm(test_output_folder_path)
     validate_arguments(arguments)
-    assert mockExit.call_count > 0 and mockExit.call_args_list[0].args[0] == -1
+    assert mockExit.call_count > 0 and mockExit.call_args_list[0].args[0] != 0
 
-    # Input file missing
-    os.mkdir(test_output_folder_path)
+
+@patch("sys.exit")
+def test_validate_arguments_no_input_file(mockExit):
     rm(test_input_file_name)
     validate_arguments(arguments)
-    assert mockExit.call_args_list[1].args[0] == -1
+    assert mockExit.call_count > 0 and mockExit.call_args_list[0].args[0] != 0
 
-    # Input file is a directory
+
+@patch("sys.exit")
+def test_validate_arguments_input_file_is_directory(mockExit):
+    rm(test_input_file_name)
     os.mkdir(test_input_file_name)
     validate_arguments(arguments)
-    assert mockExit.call_args_list[2].args[0] == -1
+    assert mockExit.call_count > 0 and mockExit.call_args_list[0].args[0] != 0
 
-    # Output folder is a file
-    rm(test_input_file_name)
-    with open(test_input_file_name, "w") as f:
-        f.write(test_input_file_content)
+
+@patch("sys.exit")
+def test_validate_arguments_output_folder_is_file(mockExit):
     rm(test_output_folder_path)
-    with open(test_output_folder_path, "w") as f:
-        f.write(test_output_folder_path)
+    open(test_output_folder_path, "a").close()
     validate_arguments(arguments)
-    assert mockExit.call_args_list[3].args[0] == -1
+    assert mockExit.call_count > 0 and mockExit.call_args_list[0].args[0] != 0
